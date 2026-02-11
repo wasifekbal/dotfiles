3042ebff057563e5e0460c2025f3038c9aad496d
wasifekbal <wasifekbal27@gmail.com>
2026-02-11 13:54:21 +0530

    Rofi changes for wayland

--- .config/rofi/scripts/powermenu/powermenu.sh
+++ .config/rofi/scripts/powermenu/powermenu.sh
@@ -3,6 +3,8 @@
 # Options
 poweroff=' Poweroff';
 reboot=' Reboot';
+hard_reboot=" Hard reboot"
+firmware='󰍛 Firmware';
 lock=' Lock';
 suspend='󰒲 Suspend';
 logout='󰍃 Logout';
@@ -11,21 +13,32 @@ logout='󰍃 Logout';
 ROFI_THEME="~/.config/rofi/scripts/powermenu/power.rasi"
 
 uptime=$(uptime -p | cut -b 4- -)
-options="$poweroff\n$reboot\n$lock\n$suspend\n$logout"
+options="$poweroff\n$reboot\n$hard_reboot\n$firmware\n$lock\n$suspend\n$logout"
 
 choice=$(echo -e $options | rofi -dmenu -p "  " -mesg "Uptime: $uptime" -i -theme $ROFI_THEME);
 
 case "$choice" in
-  $poweroff) poweroff ;;
-  $reboot) reboot ;;
-  $lock)
-      if which "screenlock" >/dev/null 2>&1 && which "betterlockscreen" >/dev/null 2>&1; then
-          screenlock;
-      else
-          xdg-screensaver lock
-      fi
-  ;;
-  $suspend) systemctl suspend ;;
-  $logout) pkill -KILL -u $(whoami);;
-  *) exit 1 ;;
+    $poweroff) poweroff ;;
+    $reboot) reboot ;;
+    $hard_reboot) pkexec "echo b > /proc/sysrq-trigger";;
+    $firmware) systemctl reboot --firmware-setup;;
+    $lock) xdg-screensaver lock ;;
+    $suspend) systemctl suspend ;;
+    $logout) pkill -KILL -u $(whoami);;
+    *) exit 1 ;;
 esac
+
+# case "$choice" in
+#   $poweroff) poweroff ;;
+#   $reboot) reboot ;;
+#   $lock)
+#       if which "screenlock" >/dev/null 2>&1 && which "betterlockscreen" >/dev/null 2>&1; then
+#           screenlock;
+#       else
+#           xdg-screensaver lock
+#       fi
+#   ;;
+#   $suspend) systemctl suspend ;;
+#   $logout) pkill -KILL -u $(whoami);;
+#   *) exit 1 ;;
+# esac
---
+++ .config/rofi/scripts/tools/restart_bcomp.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+killall -9 BCompare
+nohup bcompare  &> /dev/null &
